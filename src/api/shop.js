4499d304b72d58a48f24aa0bafaa2a11a7241c18
timur <asadullintimur04@gmail.com>
2022-06-04 14:49:39 +0300

    Added pagination to the product list

--- src/api/shop.js
+++ src/api/shop.js
@@ -4,11 +4,15 @@
 const _products = [
   { 'id': 1, 'title': 'iPad 4 Mini', 'price': 500.01, 'inventory': 2 },
   { 'id': 2, 'title': 'H&M T-Shirt White', 'price': 10.99, 'inventory': 10 },
-  { 'id': 3, 'title': 'Charli XCX - Sucker CD', 'price': 19.99, 'inventory': 5 }
+  { 'id': 3, 'title': 'Charli XCX - Sucker CD 1', 'price': 19.99, 'inventory': 5 },
+  { 'id': 4, 'title': 'Charli XCX - Sucker CD 2', 'price': 19.99, 'inventory': 7 },
+  { 'id': 5, 'title': 'Charli XCX - Sucker CD 3', 'price': 19.99, 'inventory': 8 },
+  { 'id': 6, 'title': 'Charli XCX - Sucker CD 4','price': 19.99, 'inventory': 1 },
+  { 'id': 7, 'title': 'Charli XCX - Sucker CD 5', 'price': 19.99, 'inventory': 3 },
+  { 'id': 8, 'title': 'Charli XCX - Sucker CD 6','price': 19.99, 'inventory': 4 },
 ]
 
-export default {
-  async getProducts () {
+export default {async getProducts () {
     await wait(100)
     return _products
   },
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="grid">
-    <product-item v-for="product in products"
-    :product="product"
-    :key="product.id"></product-item>
+    <product-item v-for="product in paginatedProducts"
+                  :product="product"
+                  :key="product.id"></product-item>
   </div>
+
+  <nav class="pagination">
+    <a href="#"
+       v-for="page in numberOfPages"
+       :key="page"
+       @click="setPage(page)"
+        :class="{
+         active: pagination.page === page
+        }">
+      {{ page }}
+    </a>
+  </nav>
 </template>
 
 <script>
@@ -23,8 +35,53 @@ export default {
   components: {
     ProductItem
   },
+
+  data() {
+    return {
+      pagination: {
+        page: 1,
+        perPage: 6
+      }
+    }
+  },
+
+  methods: {
+    setPage(page) {
+      this.pagination.page = page;
+    }
+  },
+
+  computed: {
+    paginatedProducts() {
+      let from = (this.pagination.page - 1) * this.pagination.perPage;
+      let to = from + this.pagination.perPage;
+
+      return this.products.slice(from, to)
+    },
+
+    numberOfPages() {
+      return Math.ceil(this.products.length / this.pagination.perPage)
+    }
+  }
 }
 </script>
 
 <style scoped>
+.pagination {
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+  gap: 10px;
+}
+
+.pagination > a {
+  padding: 7px 12px;
+  border-radius: 3px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+}
+
+.pagination > a.active {
+  background-color: steelblue;
+  color: #fff;
+}
 </style>
